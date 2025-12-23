4f9fab9a880074ec529843ee4c2835c16141ff02
LeoNarvaez2004 <leonardo_8.8@live.com>
2025-12-23 13:22:55 -0500

    Pruebas Clientes Front End

--- lab1.angular/src/app/app.spec.ts
+++ lab1.angular/src/app/app.spec.ts
@@ -1,10 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
 
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideRouter } from '@angular/router';
+
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideRouter([])
+      ]
     }).compileComponents();
   });
 
@@ -14,11 +23,5 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Laboratiorio1!');
-  });
+
 });
